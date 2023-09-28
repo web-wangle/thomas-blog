47e83b2296bdabe90fbbec614e8b244865335c83
thomas.wang <thomas.wang@klook.com>
2023-09-28 15:16:21 +0800

    feat: 上传打包后的文件

--- src/.vuepress/.temp/internal/siteData.js
+++ src/.vuepress/.temp/internal/siteData.js
@@ -1,1 +1,1 @@
-export const siteData = JSON.parse("{\"base\":\"/\",\"lang\":\"zh-CN\",\"title\":\"Thomas小站\",\"description\":\"Thomas的博客小站\",\"head\":[],\"locales\":{}}")
+export const siteData = JSON.parse("{\"base\":\"/thomas-blog/\",\"lang\":\"zh-CN\",\"title\":\"Thomas小站\",\"description\":\"Thomas的博客小站\",\"head\":[],\"locales\":{}}")
